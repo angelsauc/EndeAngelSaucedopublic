0eb4430e4006848dc17d47f8a4065ca15822c929
angelsauc <148342369+angelsauc@users.noreply.github.com>
2024-04-26 14:08:10 +0200

    Add files via upload

---
+++ AgendaConArrray.java
@@ -0,0 +1,56 @@
+package agenda;
+
+import java.util.Arrays;
+
+public class AgendaConArrray {
+//Atributos
+	private Contacto[] contactos;
+
+	//constructor para preparar una agenda vacía
+	public AgendaConArrray() {
+		this.contactos = new Contacto[200];
+	}
+	
+	public AgendaConArrray(int tamaño) {
+		this.contactos = new Contacto[tamaño];
+	}
+	
+	
+	//constructor que recibe un array ya preparado
+public AgendaConArrray(Contacto[] contactos) {
+	super();
+	this.contactos = contactos;
+}
+
+public Contacto[] getContactos() {
+	return contactos;
+}
+
+public void setContactos(Contacto[] contactos) {
+	this.contactos = contactos;
+}
+	
+//método para buscar un contacto por nombre
+public Contacto getContacto(String nombre) {
+	for (Contacto contacto : this.contactos) {
+		if (contacto.getNombre().equals(nombre)) {
+			return contacto;
+		}
+	}
+	return null;
+}
+
+//método para eliminar un contacto, pasando nombre
+public boolean removeContacto(String nombre) {
+for (int i = 0; i < contactos.length; i++) {
+	if (contactos[i].getNombre().equals(nombre)) {
+		contactos[i] = null; //borro el contacto
+	    return true;
+	}
+}
+return false;
+}
+
+//Faltan muchos métodos todavía
+	
+}
